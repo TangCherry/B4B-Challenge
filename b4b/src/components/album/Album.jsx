cd079cd1d67faa55f041b27b1a887d4fd75513fd
TangCherry <danaee16@gmail.com>
2020-11-04 15:12:15 -0600

    Adding a few changes to the album component

--- b4b/src/components/album/Album.jsx
+++ b4b/src/components/album/Album.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react';
-import { Row, Col, Typography } from 'antd';
-import meteora from '../../assets/images/meteora.jpg';
-import lol from '../../assets/images/1999.jpg';
-import spit from '../../assets/images/spit it out.jpg';
+import { Row, Col, Typography, Card } from 'antd';
 import '../../assets/styles/album.css';
 
 const { Title } = Typography;
@@ -11,26 +8,53 @@ function Album() {
     return (
         <>
         <Row>
-            <Col span={8}>
-                <img 
-                className="album"
-                src={meteora}
-                />
+            <Col span={8} style={{ paddingTop: 15, paddingRight: 15, paddingLeft: 15 }}>
+                <Card
+                hoverable
+                cover={
+                    <img 
+                    className="album"
+                    alt="meteora"
+                    src={
+                        "https://images-na.ssl-images-amazon.com/images/I/71-nhMkC-vL._SX522_.jpg"
+                    }
+                    />
+                }
+                >
                 <Title className="artist" level={4}>Linkin Park</Title>
+                </Card>
             </Col>
-            <Col span={8}>
-                <img 
-                className="album"
-                src={lol}
-                />
-                <Title className="artist" level={4}>Love of Lesbian</Title>
+            <Col span={8} style={{ paddingTop: 15, paddingRight: 15, paddingLeft: 15 }}>
+            <Card
+                hoverable
+                cover={
+                    <img 
+                    className="album"
+                    alt="meteora"
+                    src={
+                        "https://www.elquintobeatle.com/wp-content/uploads/2016/03/love-of-lesbian-1999-o-como-generar-incendios-de-nieve-con-una-lupa-enfocada-a-la-luna-1.jpg"
+                    }
+                    />
+                }
+                >
+                    <Title className="artist" level={4}>Love of Lesbian</Title>
+                </Card>
             </Col>
-            <Col span={8}>
-                <img 
-                className="album"
-                src={spit}
-                />
-                <Title className="artist" level={4}>Solar</Title>
+            <Col span={8} style={{ paddingTop: 15, paddingRight: 15, paddingLeft: 15 }}>
+            <Card
+                hoverable
+                cover={
+                    <img 
+                    className="album"
+                    alt="meteora"
+                    src={
+                        "https://upload.wikimedia.org/wikipedia/en/0/09/Solar_-_Spit_it_Out.jpg"
+                    }
+                    />
+                }
+                >
+                    <Title className="artist" level={4}>Solar</Title>
+                </Card>
             </Col>
         </Row>
         </>
